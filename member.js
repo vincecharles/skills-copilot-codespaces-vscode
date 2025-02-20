e650793d9115690ec6ddbd9fc50f78b01cd4784d
Vince Charles de Guzman <156733754+vincecharles@users.noreply.github.com>
2025-02-20 03:15:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,28 @@
+function skillsMember() {
+  return [
+    {
+      name: 'C++',
+      level: 5
+    },
+    {
+      name: 'Java',
+      level: 5
+    },
+    {
+      name: 'Python',
+      level: 5
+    },
+    {
+      name: 'JavaScript',
+      level: 5
+    },
+    {
+      name: 'HTML',
+      level: 5
+    },
+    {
+      name: 'CSS',
+      level: 5
+    }
+  ];
+}
