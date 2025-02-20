84bca0c9f5e101a52a70e20136deb8a5c916b1fb
Vince Charles de Guzman <156733754+vincecharles@users.noreply.github.com>
2025-02-20 03:30:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,42 +1,38 @@
-// Create Web server
+//create web server
 
-// Express is a Node.js web application framework
-const express = require('express');
-const app = express();
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-// body-parser extract the entire body portion of an incoming request stream and exposes it on req.body
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var server = http.createServer(function(request, response) {
+    var path = url.parse(request.url).pathname;
 
-// Allow cross-origin requests
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+    switch(path) {
+        case '/':
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write('hello world');
+            response.end();
+            break;
+        case '/comments':
+            fs.readFile(__dirname + path + '.json', function(error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write('opps this doesn\'t exist - 404');
+                    response.end();
+                } else {
+                    response.writeHead(200, {'Content-Type': 'application/json'});
+                    response.write(data);
+                    response.end();
+                }
+            });
+            break;
+        default:
+            response.writeHead(404);
+            response.write('opps this doesn\'t exist - 404');
+            response.end();
+            break;
+    }
 });
 
-// Array of comments
-let comments = [
-  { id: 1, author: 'Adam', text: 'I love your post' },
-  { id: 2, author: 'Dave', text: 'Nice work' },
-  { id: 3, author: 'John', text: 'How can I do this?' },
-  { id: 4, author: 'Adam', text: 'Where can I buy this?' },
-  { id: 5, author: 'Dave', text: 'I want to learn more' },
-];
-
-// Get all comments
-app.get('/comments', (req, res) => {
-  res.send(comments);
-});
-
-// Get comment by id
-app.get('/comments/:id', (req, res) => {
-  const comment = comments.find(c => c.id === parseInt(req.params.id));
-  if (!comment) return res.status(404).send('The comment with the given ID was not found.');
-  res.send(comment);
-});
-
-// Post a comment
-app.post('/comments', (req, res) => {
-  const comment = {
-    id: comments.length + 1,
+server.listen(8001);
+console.log('Listening on port 8001...');
